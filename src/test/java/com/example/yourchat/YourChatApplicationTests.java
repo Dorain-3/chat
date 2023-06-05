3ad4035cb26c5c5672936d1bc58541acd95f0c8d
name <3174950356@qq.com>
2023-06-05 12:14:09 +0800

    1

---
+++ src/test/java/com/example/yourchat/YourChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yourchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
